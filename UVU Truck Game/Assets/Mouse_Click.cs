53a8449cb316199569b74d978b0aac15e6d6eba3
Andrew Wright <10879159@my.uvu.edu>
2021-02-20 20:39:24 -0700

    Mouse Click
    I'm leaving the code in this time, just commenting it out, because it's giving me compiler errors. Lots of them. I don't understand this anymore.

---
+++ UVU Truck Game/Assets/Mouse_Click.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Click : MonoBehaviour
+{
+    /*
+    void OnMouseDown ()
+    {
+        rigidbody.AddForce(-transform.forward * 500f);
+        rigidbody.useGravity = true;
+    }
+    */
+}
