57d0a1d4b347fc00ec4988ba1ee6c4ddec306074
Andrew Wright <10879159@my.uvu.edu>
2021-02-20 20:28:49 -0700

    Get Axis
    This script broke everything else, so I deleted it. I'm committing a comment, because I don't know what else to do. I'm just trying to stop crying long enough to finish this assignment.

---
+++ UVU Truck Game/Assets/Get_Axis.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Get_Axis : MonoBehaviour
+{
+    //Everything breaks when I do this code, so I'm not putting it in. But I have to commit something, so here's this comment.
+}
