30a007f1c7f52fb3352ed58a32c98e4c51f3f1bc
Andrew Wright <10879159@my.uvu.edu>
2021-03-02 15:40:56 -0700

    Moving the Car
    This moves the car forward. Probably going to change it soon.

--- Project 1/Assets/Scripts/PlayerController.cs
+++ Project 1/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward.
+	transform.Translate(0, 0, 1);
     }
 }
