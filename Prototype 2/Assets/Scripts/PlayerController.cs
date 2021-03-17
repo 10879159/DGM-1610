86302ec3a3603454bc117d5117bbeb05af885d59
Andrew Wright <10879159@my.uvu.edu>
2021-03-16 20:01:54 -0600

    Added Prefab
    We're no strangers to looove... You know the rules, and so do I... A full commit is what I'm thinkin' of... You wouldn't get this from Any Other Guy... IIIII just want to tell you how I'm feelin'... Wanna make you understand, Never Gonna Give You Up, Never Gonna Let You Down, Never Gonna Run Around And Desert You. Never Gonna Make You Cry, Never Gonna Say Goodbye, Never Gonna Tell A Lie, And Hurt You.

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
     private Vector3 leftBound;
     private Vector3 rightBound;
@@ -31,5 +32,10 @@ public class PlayerController : MonoBehaviour
 	// Move player based on input
         horizontalInput = Input.GetAxis("Horizontal");
 	transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+	// Get Space Input
+	if (Input.GetKeyDown(KeyCode.Space)) {
+		// Launch Projectile
+		
+	}
     }
 }
