436ec0a4b3b63e58a0eb74dc9c5b76d3a06eff66
Andrew Wright <10879159@my.uvu.edu>
2021-03-17 18:46:36 -0600

    Spreading
    The Cows and Moose and Chickens spread out now and do not clog each others space. Such good little animals! I want to eat their flesh.

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,19 +7,23 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
 
     private Vector3 animalSpawnLocation;
+    private float spawnRangeX = 10.0f;
+    private float spawnZ = 20.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        animalSpawnLocation = new Vector3(0, 0, 20);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnZ);
+	if (Input.GetKeyDown(KeyCode.S)) {
 		int animalIndex = Random.Range(0, animalPrefabs.Length);
-		Instantiate(animalPrefabs[animalIndex], animalSpawnLocation, animalPrefabs[animalIndex].transform.rotation);
+		Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
 	}
     }
 }
