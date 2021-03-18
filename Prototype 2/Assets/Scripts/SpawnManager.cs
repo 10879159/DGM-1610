185b590c0a9e14088240c7b6c71062dcbc069a6c
Andrew Wright <10879159@my.uvu.edu>
2021-03-17 18:35:53 -0600

    Many Chickens
    Pressing S makes lots of chickens. They are good gigantic chickens. I like chickens.

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public int animalIndex;
+    private Vector3 animalSpawnLocation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animalSpawnLocation = new Vector3(0, 0, 20);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S)) {
+		Instantiate(animalPrefabs[animalIndex], animalSpawnLocation, animalPrefabs[animalIndex].transform.rotation);
+	}
     }
 }
