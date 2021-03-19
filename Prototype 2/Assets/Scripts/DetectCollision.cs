00c22745c9eef7011bf361581295d4f0e2d7c627
Andrew Wright <10879159@my.uvu.edu>
2021-03-18 18:42:02 -0600

    Animals Die
    The animals now die if you hit them in the face with a blunt object. They do it in a very dramatic way, which I am proud of. Also, they go slow enough that you can actually hit them all.

---
+++ Prototype 2/Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    private float dying = 0f;
+    private float dieCounter = 0f;
+    public float deathSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if (dying == 1) {
+		dieCounter += 1;
+		transform.Rotate(Vector3.forward * deathSpeed * Time.deltaTime);
+	}
+	if (dieCounter == 30) {
+		Destroy(gameObject);
+	}
+    }
+
+    void OnTriggerEnter(Collider other) {
+	Destroy(other.gameObject);
+	dying = 1;
+    }
+}
