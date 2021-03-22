1cf8d4f5cb8b06365da5d458fe11ee8a9a089eb9
Andrew Wright <10879159@my.uvu.edu>
2021-03-22 17:57:08 -0600

    3.1
    I'm having a bad day, so I just did this as fast as possible.

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    private Rigidbody PlayerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody>();
+	Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+		PlayerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+		isOnGround = false;
+	}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+	isOnGround = true;
+    }
+}
