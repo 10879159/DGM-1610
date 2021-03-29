7cf88f191da47f37946f9037272253a0e482d566
Andrew Wright <10879159@my.uvu.edu>
2021-03-29 16:47:04 -0600

    Ball Bounces
    Not off the players paddle, but it bounces of the walls. And spins a lot, because I couldn't figure out how to do it without a rigidbody.

---
+++ Andrew- Personal Project/Assets/Scripts/BallController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private CharacterController ballMover;
+    private Vector3 ballDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballMover = GetComponent<CharacterController>();
+	ballDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ballMover.Move(ballDirection * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collidee) {
+	if (collidee.gameObject.tag == "verticalBoxPart") {
+		ballDirection = new Vector3(ballDirection.x, ballDirection.y, -ballDirection.z);
+		Debug.Log("Hit wall!");
+	} else if (collidee.gameObject.tag == "horizontalBoxPart") {
+		ballDirection = new Vector3(ballDirection.x, -ballDirection.y, ballDirection.z);
+	}
+    }
+}
