719c4b9b3b55f29a36660e116e59e51c58fade59
Andrew Wright <10879159@my.uvu.edu>
2021-03-29 15:56:16 -0600

    Changing My Game
    I had an idea for what I want to do, so I ripped out everything and started over. This will actually be a thing, I promise. Also, I added a character controller to my player.

---
+++ Andrew- Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private CharacterController control;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	direction = new Vector3(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        control.Move(direction);
+    }
+}
