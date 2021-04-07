ed3e9ea07fecbe2c17edb63303d0194a19f61ed1
Andrew Wright <10879159@my.uvu.edu>
2021-04-06 20:30:10 -0600

    Stuff

--- Andrew- Personal Project/Assets/Scripts/FollowBallX.cs
+++ Andrew- Personal Project/Assets/Scripts/FollowBallX.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class FollowBallX : MonoBehaviour
 {
     private Transform ballInfo;
+    public ResetBall newBallScript;
 
     // Start is called before the first frame update
     void Start()
     {
         ballInfo = GameObject.Find("Ball").transform;
+	newBallScript = GameObject.Find("Ball").GetComponent<ResetBall>();
     }
 
     // Update is called once per frame
     void Update()
     {
+	if (newBallScript.newBall) {
+		ballInfo = GameObject.Find("Ball").transform;
+		newBallScript = GameObject.Find("Ball").GetComponent<ResetBall>();
+		newBallScript.newBall = false;
+	}
         transform.Translate(ballInfo.position.x - transform.position.x, 0, 0);
     }
 }
--- Andrew- Personal Project/Assets/Scripts/ResetBall.cs
+++ Andrew- Personal Project/Assets/Scripts/ResetBall.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructor : MonoBehaviour
+public class ResetBall : MonoBehaviour
 {
+    public GameObject ballPrefab;
+    public bool newBall = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(-transform.position.x, 25 - transform.position.y, -transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.x > 51 || transform.position.x < -51) {
+		Instantiate(ballPrefab);
+		newBall = true;
 		Destroy(gameObject);
 	}
     }
