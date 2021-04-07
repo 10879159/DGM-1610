b993fa402ea83edada468dc21e763cb64889c426
Andrew Wright <10879159@my.uvu.edu>
2021-04-07 17:43:42 -0600

    Hooked up Score
    It is now hooked up to a variable, which I will change soon.

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-
     public float spawnRate = 5.0f;
+    public TextMeshProUGUI scoreText;
+
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+	score = 0;
+	scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
