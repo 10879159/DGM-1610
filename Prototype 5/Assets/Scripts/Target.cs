1f156f0f6537b03c22773fa2761c7d91bdbda6f7
Andrew Wright <10879159@my.uvu.edu>
2021-04-07 17:04:26 -0600

    Food Flies
    It flies in the air and spins around.

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+	targetRb.AddForce(Vector3.up * Random.Range(15, 19), ForceMode.Impulse);
+	targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10,10), ForceMode.Impulse);
+	transform.position = new Vector3(Random.Range(-4,4), -6);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	
     }
 }
