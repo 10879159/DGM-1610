018f1f12fd3f9d74855a44c5f99f06d1ddd09360
Andrew Wright <10879159@my.uvu.edu>
2021-04-13 19:16:50 -0600

    Removed Object Pooling
    My version works better, and personally, I think it looks cooler too.

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,49 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float horizontalInput;
-    private float speed = 20.0f;
-    private float xRange = 20;
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
     public GameObject projectilePrefab;
 
+    private Vector3 leftBound;
+    private Vector3 rightBound;
+    private Vector3 fixTheBone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	leftBound = new Vector3(-xRange, transform.position.y, transform.position.z);
+	rightBound = new Vector3(xRange, transform.position.y, transform.position.z);
+	fixTheBone = new Vector3(0f, 1.5f, -5f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // Check for left and right bounds
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-
-        // Player movement left to right
+	// Keep player in bounds
+	if (transform.position.x < -xRange) {
+		transform.position = leftBound;
+	}
+	if (transform.position.x > xRange) {
+		transform.position = rightBound;
+	}
+	// Move player based on input
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // No longer necessary to Instantiate prefabs
-            // Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-
-            // Get an object object from the pool
-            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
-            if (pooledProjectile != null)
-            {
-                pooledProjectile.SetActive(true); // activate it
-                pooledProjectile.transform.position = transform.position; // position it at player
-            }
-        }
-
-
-
+	transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+	// Get Space Input
+	if (Input.GetKeyDown(KeyCode.Space)) {
+		// Launch Projectile
+		// Debug.Log(transform.position);
+		Instantiate(projectilePrefab, transform.position + fixTheBone, projectilePrefab.transform.rotation);
+	}
     }
 }
