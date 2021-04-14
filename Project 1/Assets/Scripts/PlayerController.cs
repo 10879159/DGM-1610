4db9c5fdc27ed94a251c9f23b5cfbc276f48d076
Andrew Wright <10879159@my.uvu.edu>
2021-04-13 19:32:12 -0600

    Made Physics Worse
    It's supposed to be better, but the tank just bounces and flips over now.

--- Project 1/Assets/Scripts/PlayerController.cs
+++ Project 1/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Variables
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 20.0f;
     [SerializeField] float turnSpeed = 45.0f;
     private float horizontalInput;
     private float verticalInput;
+    private Rigidbody playerRb;
+
+    void Start()
+    {
+	playerRb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +23,7 @@ public class PlayerController : MonoBehaviour
 	horizontalInput = Input.GetAxis("Horizontal");
 	verticalInput = Input.GetAxis("Vertical");
         // Move the vehicle forward.
-	transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+	playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower * Time.deltaTime);
 	transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput * verticalInput);
     }
 }
