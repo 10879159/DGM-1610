9e33128c872405697ebcd96ddda503038d403f8f
Andrew Wright <10879159@my.uvu.edu>
2021-04-29 15:00:51 -0600

    Scriptable Objects!
    Not quite sure what I did, and I'm pretty sure it doesn't work. I'm just feeling kinda blah today.

---
+++ Project 1/Assets/Scripts/FloatData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public float value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project 1/Assets/Scripts/IntData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public int value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
