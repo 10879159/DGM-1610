5b022fc96f61d622e22d2a424f12593b3b69281e
Andrew Wright <10879159@my.uvu.edu>
2021-04-29 15:08:10 -0600

    Vector3 scriptable Object!
    For some reason only the float one appears in the menu, even though they all have the [CreateAssetMenu] thing at the top.

---
+++ Project 1/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public Vector3 vector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
